07e56abe27910af4264b09634f3d64002ecdbc7d
hyunjin1012 <hyunjin1012@gmail.com>
2025-01-14 16:10:47 -0800

    edit main.py

--- main.py
+++ main.py
@@ -1,2 +1,85 @@
-import tiktoken
-print(tiktoken)
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.storage import LocalFileStore
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
+
+llm = ChatOpenAI(
+    temperature=0.1,
+)
+
+cache_dir = LocalFileStore("./.cache/")
+
+splitter = CharacterTextSplitter.from_tiktoken_encoder(
+    separator="\n",
+    chunk_size=600,
+    chunk_overlap=100,
+)
+loader = UnstructuredFileLoader("./files/Dr._Stepita_eye_contact.doc.pdf")
+
+docs = loader.load_and_split(text_splitter=splitter)
+
+embeddings = OpenAIEmbeddings()
+
+cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+
+vectorstore = FAISS.from_documents(docs, cached_embeddings)
+
+retriever = vectorstore.as_retriever()
+
+
+map_doc_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Use the following portion of a long document to see if any of the text is relevant to answer the question. Return any relevant text verbatim. If there is no relevant text, return : ''
+            -------
+            {context}
+            """,
+        ),
+        ("human", "{question}"),
+    ]
+)
+
+map_doc_chain = map_doc_prompt | llm
+
+
+def map_docs(inputs):
+    documents = inputs["documents"]
+    question = inputs["question"]
+    return "\n\n".join(
+        map_doc_chain.invoke(
+            {"context": doc.page_content, "question": question}
+        ).content
+        for doc in documents
+    )
+
+
+map_chain = {
+    "documents": retriever,
+    "question": RunnablePassthrough(),
+} | RunnableLambda(map_docs)
+
+final_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Given the following extracted parts of a long document and a question, create a final answer. 
+            If you don't know the answer, just say that you don't know. Don't try to make up an answer.
+            ------
+            {context}
+            """,
+        ),
+        ("human", "{question}"),
+    ]
+)
+
+chain = {"context": map_chain, "question": RunnablePassthrough()} | final_prompt | llm
+
+answer = chain.invoke("What should I do when my dog gets better at making eye contact?")
+print(answer.content)

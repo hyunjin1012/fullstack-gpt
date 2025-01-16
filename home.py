ade45a74e9ed580f2d1e4bff9bc06811725045b1
hyunjin1012 <hyunjin1012@gmail.com>
2025-01-15 17:15:40 -0800

    add document gpt

--- home.py
+++ home.py
@@ -1,4 +1,20 @@
 import streamlit as st
 
-st.title("Hi")
+st.title("Hi, I'm FullstackGPT")
 
+st.markdown(
+    """
+# Hello!
+            
+Welcome to my FullstackGPT Portfolio!
+            
+Here are the apps I made:
+            
+- [ ] [DocumentGPT](/DocumentGPT)
+- [ ] [PrivateGPT](/PrivateGPT)
+- [ ] [QuizGPT](/QuizGPT)
+- [ ] [SiteGPT](/SiteGPT)
+- [ ] [MeetingGPT](/MeetingGPT)
+- [ ] [InvestorGPT](/InvestorGPT)
+"""
+)
